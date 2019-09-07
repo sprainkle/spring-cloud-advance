99145d1bd6f7ec639be758c80728685398fa27ee
sprainkle <50177342+sprainkle@users.noreply.github.com>
2019-09-07 10:52:21 +0800

    fix: optimized IpUtil.
    Because there is no unsigned integer in java, IpUtil#ipToInt(String) will return a negative number possibly.
    Now you should use IpUtil#ipToLong(String).

--- spring-cloud-advance-common/spring-cloud-advance-common-core/src/main/java/com/sprainkle/spring/cloud/advance/common/core/util/IpUtil.java
+++ spring-cloud-advance-common/spring-cloud-advance-common-core/src/main/java/com/sprainkle/spring/cloud/advance/common/core/util/IpUtil.java
@@ -48,7 +48,9 @@ public class IpUtil {
      * </p>
      * @param ip
      * @return
+     * @deprecated java 没有无符号整型. 请使用 {@link #ipToLong(String)}
      */
+    @Deprecated
     public static Integer ipToInt(String ip) {
         String[] ips = ip.split("\\.");
         int ipFour = 0;
@@ -58,6 +60,7 @@ public class IpUtil {
             //这里应该用+也可以,但是位运算更快
             ipFour = (ipFour << 8) | ip4a;
         }
+
         return ipFour;
     }
 
@@ -65,7 +68,9 @@ public class IpUtil {
      * 使用int表示的ip地址, 转换成字符串的ip
      * @param ip 使用int表示的ip地址
      * @return
+     * @deprecated java 没有无符号整型. 请使用 {@link #longToIp(long)}
      */
+    @Deprecated
     public static String intToIp(Integer ip) {
         //思路很简单，每8位拿一次，就是对应位的IP
         StringBuilder sb = new StringBuilder();
@@ -73,8 +78,46 @@ public class IpUtil {
             int ipa = (ip >> (8 * i)) & (0xff);
             sb.append(".").append(ipa);
         }
-        sb.delete(0, 1);
-        return sb.toString();
+
+        return sb.substring(1);
+    }
+
+    /**
+     * <p>ip使用long表示.</p>
+     * <p>
+     *     ip有4段, 每段最大值为255, 即 2^8 - 1, 刚好是一个字节能表示的最大值,
+     *     所以4个字节的int刚好能用来表示一个ip地址.
+     * </p>
+     * @param ip
+     * @return
+     */
+    public static long ipToLong(String ip) {
+        String[] ips = ip.split("\\.");
+        long ipFour = 0;
+        //因为每个位置最大255，刚好在2进制里表示8位
+        for(String ip4: ips){
+            int ip4a = Integer.parseInt(ip4);
+            //这里应该用+也可以,但是位运算更快
+            ipFour = (ipFour << 8) | ip4a;
+        }
+
+        return ipFour;
+    }
+
+    /**
+     * 使用long表示的ip地址, 转换成字符串的ip
+     * @param ip 使用int表示的ip地址
+     * @return
+     */
+    public static String longToIp(long ip) {
+        //思路很简单，每8位拿一次，就是对应位的IP
+        StringBuilder sb = new StringBuilder();
+        for(int i = 3; i >= 0; i--){
+            int ipa = (int) ((ip >> (8 * i)) & (0xff));
+            sb.append(".").append(ipa);
+        }
+
+        return sb.substring(1);
     }
 
 }
